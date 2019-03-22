c94510482964d31cbd2692fcdb34095a79220e2e
Edwin Didier Méndez Rojas - Ceiba Software <edwin.mendez@ceiba.com.co>
2019-03-22 14:06:22 -0500

    setting api-gateway

---
+++ src/main/java/server/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import server.filter.PreFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+}
